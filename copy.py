0280e3a41dcdcc46f64f0faffae6d753e05fdd3f
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2021-02-21 00:45:32 +0530

    Add files via upload

---
+++ copy.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+file_name = input("Enter the exact location of file or simply name, if this script and your file exists in same directory.\n")
+f = open(file_name,'r')
+data = f.read()
+
+print(" Click on the place where you want us to type your data for you under 3 seconds.")
+time.sleep(3)
+for i in data:
+    pyautogui.typewrite(i)
+    time.sleep(0.005)
